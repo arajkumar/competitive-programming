a0eb8fc7ba9e53524cf0e4f039cd0f5acd9b9693
Arunprasad Rajkumar <ararunprasad@gmail.com>
2018-04-05 21:42:22 +0530

    Refactor Tree impl

--- brushup/tree.cpp
+++ brushup/tree.cpp
@@ -1,54 +1,92 @@
+// clang++ -std=c++14
+//
 #include <iostream>
 #include <memory>
 
-template <typename T> struct TreeNode {
-    T data;
+template <typename T> struct Tree {
 
-    TreeNode<T>* left { };
-    TreeNode<T>* right { };
+    struct Node {
+        T data;
 
-    TreeNode(T data) : data(data)
-    {
-        // std::cout << __func__ << " data:" << data << std::endl;
-    }
+        Node* left { };
+        Node* right { };
+
+        Node(const T& data) : data(data)
+        {
+            // std::cout << __func__ << " data:" << data << std::endl;
+        }
+
+        void insert(const T& data)
+        {
+            if (data < this->data) {
+                if (!this->left) {
+                    this->left = new Node { data };
+                    return;
+                }
+                left->insert(data);
+            } else {
+                if (!this->right) {
+                    this->right = new Node { data };
+                    return;
+                }
+                right->insert(data);
+            }
+
+        }
+
+        void visit(std::function<void(const T&)>& func)
+        {
+            if (left)
+                left->visit(func);
+
+            func(data);
+
+            if (right)
+                right->visit(func);
+        }
+    };
+
+    Node* head {};
+
+    Tree() = default;
 
     void insert(const T& data)
     {
-        if (data < this->data) {
-            if (!left) {
-                left = new TreeNode<T> { data };
-                return;
-            }
-            left->insert(data);
-        } else {
-            if (!right) {
-                right = new TreeNode<T> { data };
-                return;
-            }
-            right->insert(data);
+        if (!head) {
+            this->head = new Node { data };
+            return;
         }
+        head->insert(data);
     }
 
-    void print() const
+    void visit(std::function<void(const T&)> func) const
     {
-        if (left)
-            left->print();
-        std::cout << data << std::endl;
-        if (right)
-            right->print();
+        if (!head)
+            return;
+        head->visit(func);
     }
-
-    using uptr = std::unique_ptr<TreeNode<T>>;
 };
 
 int main()
 {
-    using uptr_i = TreeNode<int>::uptr;
-    uptr_i head = std::make_unique<TreeNode<int>>(1);
-    head->insert(2);
-    head->insert(3);
-    head->insert(100);
-    head->insert(1);
-    head->print();
+    auto tree = std::make_unique<Tree<int>>();
+    tree->insert(5);
+    tree->insert(2);
+    tree->insert(3);
+    tree->insert(100);
+    tree->insert(500);
+    tree->insert(700);
+    tree->insert(1);
+    tree->visit([](auto d) { std::cout << d <<std::endl; });
+
+    auto str_tree = std::make_unique<Tree<std::string>>();
+    str_tree->insert("f");
+    str_tree->insert("b");
+    str_tree->insert("a");
+    str_tree->insert("x");
+    str_tree->insert("k");
+    str_tree->insert("c");
+    str_tree->insert("z");
+    str_tree->visit([](auto d) { std::cout << d <<std::endl; });
     return 0;
 }
